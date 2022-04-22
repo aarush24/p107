a463586685e53048c7d2bcf8982f2a3b7c20ca15
aarush24 <82024065+aarush24@users.noreply.github.com>
2022-04-22 19:45:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function startclassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/r5Ly1heTm/model.json', modelReady);
+
+}
+
+function modelReady (){
+    classifier.classify(gotResults);
+}
